d1d8947ab02c4e62e630b77f0eac19677a5c85a8
Yasuhiro Takada <y.takada@gxp.co.jp>
2019-02-06 11:56:12 +0900

    STEP02 基本のテスト - spockframworkを実装

--- src/test/groovy/tecktalk201902/groovy/Groovy_Test_Chapter1.groovy
+++ src/test/groovy/tecktalk201902/groovy/Groovy_Test_Chapter1.groovy
@@ -1,77 +1,57 @@
-package tecktalk201902.groovy;
+package tecktalk201902.groovy
 
-import java.util.OptionalInt;
+import org.junit.Test
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import spock.lang.Specification
+import tecktalk201902.Sut_Chapter1
 
-import tecktalk201902.Sut_Chapter1;
-import tecktalk201902.testcase.Test_Chapter1;
+/* ================================================================================
+ * ((1)) spock.lang.Specification を 継承
+ * ================================================================================*/
+class Groovy_Test_Chapter1 extends Specification { // implements Test_Chapter1
 
-/**
- * Tecktalk 2019 02 後半.<br>
- * テストケース１のJunit4実装
- *
- * @author y.takada
- *
- */
-public class Groovy_Test_Chapter1 implements Test_Chapter1 {
 
-	private Sut_Chapter1 sut;
+	private Sut_Chapter1 sut
 
-	@Before
+
+/* ================================================================================
+ * ((2)). Junitいらない, ただし、＠Before相当はsetupという名前であること
+ ================================================================================*/
+//	@Before
 	public void setup() {
-		// テスト対象セットアップ
-		sut = new Sut_Chapter1() {
-			// nothing.
-		};
+		sut = new Sut_Chapter1() {}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see tecktalk201902.junit4.Test_Chapter11#テスト1_1_基本のテスト_文字列()
-	 */
-	@Override
-	@Test
+//	@Override
+//	@Test /* ((1)) */
 	public void テスト1_1_基本のテスト_文字列() {
-
-		String actual = sut.repeat("test", 10);
-		// 文字列
-		Assert.assertNotNull("結果はnullではないこと", actual);
-		Assert.assertEquals("結果は40桁であること", 40, actual.length());
+/* ================================================================================
+ * ((3)) expect:, then:ブロックに booleanを返す式を書くとassert
+ ================================================================================*/
+		// TODO: アンケート、expectedは左辺？
+		expect:
+		sut.repeat("test", 10).size() == 40
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see tecktalk201902.junit4.Test_Chapter11#テスト1_2_基本のテスト_数値()
-	 */
-	@Override
-	@Test
+//	@Override
+//	@Test /* ((1)) */
 	public void テスト1_2_基本のテスト_数値() {
-		// 数値
-		int actual = sut.sum(1, 1);
-		Assert.assertEquals("`1 + 1`は2であること", 2, actual);
+		expect:
+		sut.sum(1, 1) == 2 //`1 + 1`は2であること",
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see tecktalk201902.junit4.Test_Chapter11#テスト1_3_基本のテスト_配列()
-	 */
-	@Override
-	@Test
+//	@Override
+//	@Test /* ((1)) */
 	public void テスト1_3_基本のテスト_配列() {
-		// 配列
-		int[] fibo = sut.fibo(0, 10);
-		Assert.assertEquals("配列の要素数は10", 10, fibo.length);
-		Assert.assertArrayEquals("配列の要素比較", new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 }, fibo);
+		when:
+		int[] fibo = sut.fibo(0, 10)
+		OptionalInt max = sut.max(fibo)
 
-		OptionalInt max = sut.max(fibo);
-
-		Assert.assertTrue("配列の最大値が取得できること", max.isPresent());
-		Assert.assertEquals("初期値0, 要素数10のフィボナッチの最大値は34", 34, max.getAsInt());
+		then:
+		10 == fibo.length // "配列の要素数は10",
+		[0, 1, 1, 2, 3, 5, 8, 13, 21, 34] == fibo // "配列の要素比較"
+		max.present // "配列の最大値が取得できること"
+		34 ==  max.asInt // "初期値0, 要素数10のフィボナッチの最大値は34",
 	}
+
 }
