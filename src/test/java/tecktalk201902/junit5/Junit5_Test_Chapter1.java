008cd69b258b6e0ad4abc23765991645e9c01415
Yasuhiro Takada <y.takada@gxp.co.jp>
2019-02-07 10:06:15 +0900

    step/02 junit5, kotlin実装

--- src/test/java/tecktalk201902/junit5/Junit5_Test_Chapter1.java
+++ src/test/java/tecktalk201902/junit5/Junit5_Test_Chapter1.java
@@ -2,9 +2,15 @@ package tecktalk201902.junit5;
 
 import java.util.OptionalInt;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+/* ================================================================================
+ * ((1)) アノテーションいらないをjunit5のものにかえる
+ * import org.junit.Assert; => import static org.junit.jupiter.api.Assertions.*;
+ * import org.junit.Before; => import org.junit.jupiter.api.BeforeEach;
+ * import org.junit.Test;   => import org.junit.jupiter.api.Test;
+ * ================================================================================*/
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import tecktalk201902.Sut_Chapter1;
 import tecktalk201902.testcase.Test_Chapter1;
@@ -20,58 +26,42 @@ public class Junit5_Test_Chapter1 implements Test_Chapter1 {
 
 	private Sut_Chapter1 sut;
 
-	@Before
+	@BeforeEach
 	public void setup() {
-		// テスト対象セットアップ
 		sut = new Sut_Chapter1() {
 			// nothing.
 		};
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see tecktalk201902.junit4.Test_Chapter11#テスト1_1_基本のテスト_文字列()
-	 */
 	@Override
 	@Test
 	public void テスト1_1_基本のテスト_文字列() {
-
 		String actual = sut.repeat("test", 10);
-		// 文字列
-		Assert.assertNotNull("結果はnullではないこと", actual);
-		Assert.assertEquals("結果は40桁であること", 40, actual.length());
+/*================================================================================
+ * ((2)) org.junit.jupiter.api.Assertionsではmessageは最後の引数になった
+ ================================================================================*/
+		assertNotNull(actual, "結果はnullではないこと");
+		assertEquals(40, actual.length(), "結果は40桁であること");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see tecktalk201902.junit4.Test_Chapter11#テスト1_2_基本のテスト_数値()
-	 */
 	@Override
 	@Test
 	public void テスト1_2_基本のテスト_数値() {
-		// 数値
 		int actual = sut.sum(1, 1);
-		Assert.assertEquals("`1 + 1`は2であること", 2, actual);
+		assertEquals(2, actual, "`1 + 1`は2であること");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see tecktalk201902.junit4.Test_Chapter11#テスト1_3_基本のテスト_配列()
-	 */
 	@Override
 	@Test
 	public void テスト1_3_基本のテスト_配列() {
 		// 配列
 		int[] fibo = sut.fibo(0, 10);
-		Assert.assertEquals("配列の要素数は10", 10, fibo.length);
-		Assert.assertArrayEquals("配列の要素比較", new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 }, fibo);
+		assertEquals(10, fibo.length, "配列の要素数は10");
+		assertArrayEquals(new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 }, fibo, "配列の要素比較");
 
 		OptionalInt max = sut.max(fibo);
 
-		Assert.assertTrue("配列の最大値が取得できること", max.isPresent());
-		Assert.assertEquals("初期値0, 要素数10のフィボナッチの最大値は34", 34, max.getAsInt());
+		assertTrue( max.isPresent(), "配列の最大値が取得できること");
+		assertEquals(34, max.getAsInt(), "初期値0, 要素数10のフィボナッチの最大値は34");
 	}
 }
--- src/test/kotlin/tecktalk201902/kotlin/Kotlin_Test_Chapter1.kt
+++ src/test/kotlin/tecktalk201902/kotlin/Kotlin_Test_Chapter1.kt
@@ -1,11 +1,9 @@
 package tecktalk201902.kotlin
 
-import java.util.OptionalInt
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
+import io.kotlintest.matchers.boolean.shouldBeTrue
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
 import tecktalk201902.Sut_Chapter1
-import tecktalk201902.testcase.Test_Chapter1
 
 /**
  * Tecktalk 2019 02 後半.<br>
@@ -14,57 +12,26 @@ import tecktalk201902.testcase.Test_Chapter1
  * @author y.takada
  *
  */
-class Kotlin_Test_Chapter1 : Test_Chapter1 {
-	private val sut: Sut_Chapter1? = null
-	@Before
-	fun setup() {
-// テスト対象セットアップ
-		sut = object : Sut_Chapter1() {
-// nothing.
-		}
-	}
+class Kotlin_Test_Chapter1 : StringSpec({
 
-	/*
-         * (non-Javadoc)
-         *
-         * @see tecktalk201902.junit4.Test_Chapter11#テスト1_1_基本のテスト_文字列()
-         */
-	@Override
-	@Test
-	fun テスト1_1_基本のテスト_文字列() {
-		val actual = sut!!.repeat("test", 10)
-// 文字列
-		Assert.assertNotNull("結果はnullではないこと", actual)
-		Assert.assertEquals("結果は40桁であること", 40, actual!!.length())
-	}
+    val sut: Sut_Chapter1 = object : Sut_Chapter1 {}
 
-	/*
-         * (non-Javadoc)
-         *
-         * @see tecktalk201902.junit4.Test_Chapter11#テスト1_2_基本のテスト_数値()
-         */
-	@Override
-	@Test
-	fun テスト1_2_基本のテスト_数値() {
-// 数値
-		val actual = sut!!.sum(1, 1)
-		Assert.assertEquals("`1 + 1`は2であること", 2, actual)
-	}
+    "テスト1_1_基本のテスト_文字列" {
+        sut.repeat("test", 10).length.shouldBe(40)
+    }
 
-	/*
-         * (non-Javadoc)
-         *
-         * @see tecktalk201902.junit4.Test_Chapter11#テスト1_3_基本のテスト_配列()
-         */
-	@Override
-	@Test
-	fun テスト1_3_基本のテスト_配列() {
-// 配列
-		val fibo = sut!!.fibo(0, 10)
-		Assert.assertEquals("配列の要素数は10", 10, fibo!!.size)
-		Assert.assertArrayEquals("配列の要素比較", intArrayOf(0, 1, 1, 2, 3, 5, 8, 13, 21, 34), fibo)
-		val max = sut!!.max(fibo)
-		Assert.assertTrue("配列の最大値が取得できること", max!!.isPresent())
-		Assert.assertEquals("初期値0, 要素数10のフィボナッチの最大値は34", 34, max!!.getAsInt())
-	}
-}
+    "テスト1_2_基本のテスト_数値" {
+        // 数値
+        sut.sum(1, 1).shouldBe(2)
+    }
+
+    "テスト1_3_基本のテスト_配列" {
+        // 配列
+        val fibo = sut.fibo(0, 10)
+        fibo.size.shouldBe(10)
+        fibo.shouldBe(intArrayOf(0, 1, 1, 2, 3, 5, 8, 13, 21, 34))
+        val max = sut.max(fibo)
+        max.isPresent.shouldBeTrue()
+        max.asInt.shouldBe(34)
+    }
+})
